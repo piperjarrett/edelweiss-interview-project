d28ce392204d68d29d9a177066b6e5239443c241
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2023-08-11 13:52:08 -0600

    remove console.logs

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -2,33 +2,25 @@ import fillerImage from "../fillerImage.jpg";
 import "./Product.css";
 
 const Product = ({ product }) => {
-  console.log(product);
   const jackcoverimage = product.images?.find((image) =>
     image.uri.includes("jacket_covers")
   );
 
   const author = product.author?.split(",").reverse().join(" ");
 
-  const categories = product.category?.split("/").map((cat) => {
-    return <p>{cat}</p>;
-  });
-
   return (
     <div className="product-div" key={product.sku}>
-      <div className="media">
-        <div className="layer">
-          <p>{product.category ? product.category : "Unkown Category"}</p>
-        </div>
-        <div className="image-div">
-          <img
-            alt="cover"
-            src={jackcoverimage ? jackcoverimage.uri : fillerImage}
-          />
-        </div>
+      <div className="image-div">
+        <img
+          alt="cover"
+          src={jackcoverimage ? jackcoverimage.uri : fillerImage}
+        />
       </div>
+
       <div className="product-info">
-        <h4>{product.name ? product.name : "Unkown"}</h4>
-        <p>By: {product.author ? author : "Unkown"}</p>
+        <h3>{product.name ? product.name : "Unkown"}</h3>
+        <h4>By: {product.author ? author : "Unkown"}</h4>
+        <p>{product.category ? product.category : "Unkown Category"}</p>
       </div>
     </div>
   );
