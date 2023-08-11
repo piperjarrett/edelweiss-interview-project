13a960aa09e581b9691671a07daf80dc0c7af32b
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2023-08-11 13:44:06 -0600

    add category to product card

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,38 +1,33 @@
-import logo from "../logo.svg";
+import fillerImage from "../fillerImage.jpg";
 import "./Product.css";
 
 const Product = ({ product }) => {
+  console.log(product);
   const jackcoverimage = product.images?.find((image) =>
     image.uri.includes("jacket_covers")
   );
 
   const author = product.author?.split(",").reverse().join(" ");
-  console.log(author);
+
+  const categories = product.category?.split("/").map((cat) => {
+    return <p>{cat}</p>;
+  });
+
   return (
-    <div
-      className="product-div"
-      key={product.sku}
-      //   style={
-      //     jackcoverimage
-      //       ? {
-      //           backgroundImage: `url("${jackcoverimage.uri}")`,
-      //           backgroundPosition: "center",
-      //           backgroundSize: "cover",
-      //           backgroundRepeat: "no-repeat",
-      //         }
-      //       : {
-      //           backgroundImage: `url("${logo}")`,
-      //           backgroundPosition: "center",
-      //           backgroundSize: "cover",
-      //           backgroundRepeat: "no-repeat",
-      //         }
-      //   }
-    >
-      <div className="image-div">
-        <img src={jackcoverimage ? jackcoverimage.uri : logo} />
+    <div className="product-div" key={product.sku}>
+      <div className="media">
+        <div className="layer">
+          <p>{product.category ? product.category : "Unkown Category"}</p>
+        </div>
+        <div className="image-div">
+          <img
+            alt="cover"
+            src={jackcoverimage ? jackcoverimage.uri : fillerImage}
+          />
+        </div>
       </div>
       <div className="product-info">
-        <h4>Title: {product.fullName ? product.fullName : "Unkown"}</h4>
+        <h4>{product.name ? product.name : "Unkown"}</h4>
         <p>By: {product.author ? author : "Unkown"}</p>
       </div>
     </div>
